5ee34ae93712437b831c0c8d73764132677cdd9e
Omar <omar.k.elgarhy@gmail.com>
2022-06-16 10:00:12 +0200

    no message

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-export { ChatProvider } from "./lib/chat_provider";
-export { FirebaseChatConfigs } from "./lib/FirebaseChatConfigs";
-export { SendMessageTask, TaskUpdateEvent, TaskCompletedEvent } from "./lib/SendTask";
-export { ChatAttachment } from "./lib/models/ChatAttachment";
-export { Lobby } from "./lib/models/Lobby";
-export { Message } from "./lib/models/Message";
-export { Participant } from "./lib/models/Participant";
-export { Room } from "./lib/models/Room";
