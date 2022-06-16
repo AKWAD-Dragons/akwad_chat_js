a81d9686b262d0277249f975c22af84e2b32fffd
Omar <omar.k.elgarhy@gmail.com>
2022-06-16 17:00:13 +0200

    why did I delete this

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { ChatProvider, AttachmentTypes } from "./chat_provider";
+export { FirebaseChatConfigs } from "./FirebaseChatConfigs";
+export {SendMessageTask, TaskUpdateEvent, TaskCompletedEvent} from "./SendTask";
+export { ChatAttachment } from "./models/ChatAttachment";
+export { Lobby } from "./models/Lobby";
+export { Message } from "./models/Message";
+export { Participant } from "./models/Participant";
+export { Room } from "./models/Room";
