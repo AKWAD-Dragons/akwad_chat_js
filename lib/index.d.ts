bdfc25e7dfc051970ef0f9b2239f546bfc322e5d
dibo <sherif.eldeeb@learnovia.com>
2022-10-27 17:30:11 +0200

    build: clean the project

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { ChatProvider, AttachmentTypes } from "./chat_provider";
-export { FirebaseChatConfigs } from "./FirebaseChatConfigs";
-export { SendMessageTask, TaskUpdateEvent, TaskCompletedEvent } from "./SendTask";
-export { ChatAttachment } from "./models/ChatAttachment";
-export { Lobby } from "./models/Lobby";
-export { Message } from "./models/Message";
-export { Participant } from "./models/Participant";
-export { Room } from "./models/Room";
--- lib/models/ChatAttachment.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class ChatAttachment {
-    key: string;
-    file?: File;
-    type: string;
-    fileLink?: string;
-    constructor(key: string, type: string, file?: File, file_link?: string);
-}
